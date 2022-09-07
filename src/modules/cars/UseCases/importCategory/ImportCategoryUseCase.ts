176298844473ff0326fadf5fcf80252c59a6013d
Lucas Barbosa <luccacpm@gmail.com>
2022-09-07 02:07:21 -0300

    import

--- src/modules/cars/UseCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/UseCases/importCategory/ImportCategoryUseCase.ts
@@ -1,20 +1,62 @@
 import fs from "fs";
 import csvParse from "csv-parser";
+import { ICategoryRepository } from "../../repositories/ICategoryRepository";
+import { resolve } from "path";
+import { rejects } from "assert";
+
+interface IImportCategory {
+    name: string;
+    description: string;
+}
 
 class ImportCategoryUseCase {
+    constructor(private categoriesRepository: ICategoryRepository) {}
 
-    execute(file: Express.Multer.File ): void {
-        //leitura do arquivo 
-        const stream = fs.createReadStream(file.path);
+        loadCategories(file: Express.Multer.File ): Promise<IImportCategory[]> {
+            return new Promise((resolve, reject) => {
+            
 
-        const parseFile = csvParse();
+            const stream = fs.createReadStream(file.path);
+            const categories: IImportCategory[] = [];
 
-        //pedaços do arquivo
-        stream.pipe(parseFile)
+            const parseFile = csvParse();
 
-        parseFile.on("data", async (line) => {
+            //pedaços do arquivo
+            stream.pipe(parseFile)
+
+            parseFile
+                .on("data", async (line) => {
+                const [name,description] = line;
+                categories.push({
+                    name,
+                    description,
+                });
+            })
+              .on("end", () => {
+                resolve(categories);
+            })
+            .on("error", (err) => {
+                reject(err)
+            })
         });
+    }
+
+        async execute(file: Express.Multer.File): Promise<void> {
+            const categories = await this.loadCategories(file);
+
+            categories.map(async (category) => {
+                const { name, description } = category;
+                const existsCategory = this.categoriesRepository.findByName(name);
+
+                if (!existsCategory) {
+                    this.categoriesRepository.create({
+                        name,
+                        description,
+                    });
+                }
+            })
     }
 }
 
 export { ImportCategoryUseCase };
+
